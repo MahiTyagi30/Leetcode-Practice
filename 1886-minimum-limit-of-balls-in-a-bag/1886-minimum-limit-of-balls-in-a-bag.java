105b58ca68d2e6f212cf26ea844be5691cb11ccc
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2025-09-05 17:11:36 +0530

    Time: 91 ms (8.91%), Space: 60.7 MB (7.1%) - LeetHub

---
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean canDivide(int[] nums,int mid,int h){
+        long op=0;
+        for(int i=0;i<nums.length;i++){
+           int a = (int) Math.ceil((double) nums[i] / mid);
+            op=op+a-1;
+            if(op>h){
+            return false;
+        }
+        }
+        
+        return true;
+    }
+    public int minimumSize(int[] nums, int maxOperations) {
+        
+        int n=nums.length;
+        int mx=-1;
+        for(int i=0;i<n;i++){
+            mx=Math.max(mx,nums[i]);
+        }
+        int l=0;
+        int r=mx;
+        int res=mx;
+        while(l<=r){
+           int mid = l + (r - l) / 2;
+            if(canDivide(nums,mid,maxOperations)){
+               res = mid;   // valid, try smaller
+                r = mid - 1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return res;
+    }
+}
