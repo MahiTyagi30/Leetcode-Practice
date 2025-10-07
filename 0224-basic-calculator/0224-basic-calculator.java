5802b72c6392c11f45f09a7878a6d1bca64a6605
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2025-10-07 14:18:49 +0530

    Time: 11 ms (37.94%), Space: 44.7 MB (43.93%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -2,41 +2,45 @@ import java.util.Stack;
 
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> st = new Stack<>();
         int result = 0;
         int num = 0;
         int sign = 1;
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-
-            if (Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0'); // build multi-digit numbers
-            } else if (ch == '+') {
-                result += sign * num;
-                num = 0;
-                sign = 1;
-            } else if (ch == '-') {
-                result += sign * num;
-                num = 0;
-                sign = -1;
-            } else if (ch == '(') {
-                // Push current result and sign onto stack
-                stack.push(result);
-                stack.push(sign);
-                result = 0;
-                sign = 1;
-            } else if (ch == ')') {
-                result += sign * num;
-                num = 0;
-
-                result *= stack.pop(); // last sign
-                result += stack.pop(); // last result
+            if(Character.isDigit(ch)){
+                num=num*10+(ch-'0');
             }
-        }
+            else if(ch=='+'){
+                result += sign*num;
+                num=0;
+                sign=1;
+            }
+            else if(ch=='-'){
+                result += sign*num;
+                num=0;
+                sign=-1;
+            }
+            else if(ch=='('){
+               st.push(result);
+               st.push(sign);
+               result=0;
+               sign=1;
+            }
+            else if(ch==')'){
+              result += sign*num;
+               num = 0;
+              result *= st.pop();
+              result += st.pop();
 
-        // Add the last number
-        result += sign * num;
-        return result;
+            }
+           
+
+        }
+         result += sign*num;
+            return result;
+
+            
     }
 }
