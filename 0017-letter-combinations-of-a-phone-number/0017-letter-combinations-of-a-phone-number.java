4feca0818f73b8eaf1c9a0d8c16bbff9f64457d9
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2025-10-08 13:52:58 +0530

    Time: 0 ms (100%), Space: 42.2 MB (51.94%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,9 +1,32 @@
 import java.util.*;
 
 class Solution {
+    private void fun(List<String> result,StringBuilder sb,int index,String [] ar,String digits){
+        if(index==digits.length()){
+            result.add(sb.toString());
+            return;
+        }
+        int num=digits.charAt(index)-'0';
+        String value=ar[num-2];
+        for(int i=0;i<value.length();i++){
+            sb.append(value.charAt(i));
+            fun(result,sb,index+1,ar,digits);
+            sb.delete(index, index + 1);
+        }
+    }
  
 
     public List<String> letterCombinations(String digits) {
+        String[] ar = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> result = new ArrayList<>();
+        int l=digits.length();
+        if(l==0){
+            return result;
+        }
+        int index=0;
+        StringBuilder sb=new StringBuilder("");
+        fun(result,sb,index,ar,digits);
+        return result;
         
     }
 }
