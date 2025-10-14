24cf0745196495a0d4171aad6a8e4ae3090f3505
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2025-10-14 19:19:59 +0530

    Time: 4 ms (5.91%), Space: 50.1 MB (5.33%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,17 +1,33 @@
 class Solution {
+    // memoization code
+
+    public int fun(int [] prices,int [][] dp,int n,int buy,int idx){
+        if(idx==n){
+            return 0;
+        }
+        int profit=0;
+        if(dp[idx][buy]!=-1){
+            return dp[idx][buy];
+        }
+        if(buy==1){
+            profit=Math.max(-prices[idx]+fun(prices,dp,n,0,idx+1),0+fun(prices,dp,n,1,idx+1));
+        }
+        else{
+            profit=Math.max(prices[idx]+fun(prices,dp,n,1,idx+1),0+fun(prices,dp,n,0,idx+1));
+
+        }
+       return dp[idx][buy]=profit;
+        // return dp[idx][buy];
+
+
+    }
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        boolean ar[]=new boolean[n];
-        int buy=prices[0];
-        int profit=0;
-        for(int i=0;i<n-1;i++){
-            if(prices[i]<prices[i+1]){
-                profit=profit+prices[i+1]-prices[i];
-            }
-            
-            
+        int dp [][]=new int[n][2];
+         for (int i = 0; i < n; i++) {
+            dp[i][0] = dp[i][1] = -1;
         }
-        return profit;
+        return fun(prices,dp,n,1,0);
         
     }
 }
