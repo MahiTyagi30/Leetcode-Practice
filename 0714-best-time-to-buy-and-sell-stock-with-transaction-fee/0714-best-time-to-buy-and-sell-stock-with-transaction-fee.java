131dc381e09ee88671d8aba16d12ab1eca9d890c
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2025-10-17 10:48:14 +0530

    Time: 34 ms (22.44%), Space: 62.5 MB (27.91%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,27 +1,36 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int n = prices.length;
-        Integer[][] dp = new Integer[n][2];  // dp[i][buy]
-        return dfs(0, 1, prices, fee, dp);
+    int n=prices.length;
+    int dp[][]=new int[n][2];
+    for(int i=0;i<dp.length;i++){
+        dp[i][0]=dp[i][1]=-1;
     }
-
-    public int dfs(int i, int buy, int[] prices, int fee, Integer[][] dp) {
-        if (i == prices.length) return 0;
-        if (dp[i][buy] != null) return dp[i][buy];
-
-        int profit;
-        if (buy == 1) {
-            // Option 1: Buy
-            // Option 2: Skip
-            profit = Math.max(-prices[i] + dfs(i + 1, 0, prices, fee, dp),
-                              dfs(i + 1, 1, prices, fee, dp));
-        } else {
-            // Option 1: Sell (subtract fee)
-            // Option 2: Skip
-            profit = Math.max(prices[i] - fee + dfs(i + 1, 1, prices, fee, dp),
-                              dfs(i + 1, 0, prices, fee, dp));
-        }
-
-        return dp[i][buy] = profit;
+    return dfs(dp,prices,1,n,fee,0);
+       
     }
+public static int dfs(int[][] dp,int[] prices,int buy,int n,int fees,int idx)
+{
+    int maxprofit=0;
+    if(idx>=n){
+        return 0;
+    }
+    if(dp[idx][buy]!=-1){
+        return dp[idx][buy];
+    }
+    if(buy==1){
+      int a=-prices[idx]-fees+dfs(dp,prices,0,n,fees,idx+1);
+      int b=0+dfs(dp,prices,1,n,fees,idx+1);
+      maxprofit=Math.max(a,b);
+    }
+    else{
+         int a=prices[idx]+dfs(dp,prices,1,n,fees,idx+1);
+      int b=0+dfs(dp,prices,0,n,fees,idx+1);
+      maxprofit=Math.max(a,b);
+
+    }
+    
+    return dp[idx][buy]=maxprofit;
+}
+
+ 
 }
