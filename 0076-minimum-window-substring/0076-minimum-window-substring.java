8f84c92596790540ddfe8e7d0305dd2248c60c19
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2025-11-08 18:39:59 +0530

    Time: 22 ms (27.25%), Space: 47.6 MB (5.23%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,48 +1,45 @@
 import java.util.*;
-
 class Solution {
     public String minWindow(String s, String t) {
         if (s.length() < t.length()) return "";
       
-        HashMap<Character, Integer> m1 = new HashMap<>(); // Need
-        HashMap<Character, Integer> m2 = new HashMap<>(); // Window
-        
-        // Build need map
-        for (char ch : t.toCharArray()) {
-            m1.put(ch, m1.getOrDefault(ch, 0) + 1);
+    HashMap<Character,Integer> m1=new HashMap<>();
+    HashMap<Character,Integer> m2=new HashMap<>();
+    int l1=s.length();
+    int l2=t.length();
+    for(int i=0;i<l2;i++){
+        char ch=t.charAt(i);
+        // m1.put(map.getOrDefault(ch,0)+1);
+         m1.put(ch,m1.getOrDefault(ch,0)+1);
+    }
+    int need=m1.size();
+    int l=0;
+    int minLen = Integer.MAX_VALUE;
+    int count=0;
+    int si=-1;
+
+    for(int r=0;r<l1;r++){
+        char ch=s.charAt(r);
+        m2.put(ch,m2.getOrDefault(ch,0)+1);
+        if(m1.containsKey(ch)&&m1.get(ch).intValue()==m2.get(ch).intValue()){
+            count++;
+            // m2.put(ch,m2.getOrDefault(ch,0)-1);
         }
-        
-        int need = m1.size();  // Number of unique characters needed
-        int have = 0;          // Number of characters currently satisfied
-        int l = 0, minLen = Integer.MAX_VALUE, startIdx = 0;
-        
-        for (int r = 0; r < s.length(); r++) {
-            char ch = s.charAt(r);
-            m2.put(ch, m2.getOrDefault(ch, 0) + 1);
-            
-            // If this character is needed and now fully satisfied
-            if (m1.containsKey(ch) && m1.get(ch).intValue() == m2.get(ch).intValue()) {
-                have++;
+        while(count==need){
+            if(r-l+1<minLen){
+                minLen=r-l+1;
+                si=l;
             }
-            
-            // Try to shrink window while valid
-            while (have == need) {
-                if (r - l + 1 < minLen) {
-                    minLen = r - l + 1;
-                    startIdx = l;
-                }
-                
-                char c = s.charAt(l);
-                m2.put(c, m2.get(c) - 1);
-                
-                // If we lost a fully satisfied char
-                if (m1.containsKey(c) && m2.get(c) < m1.get(c)) {
-                    have--;
-                }
-                l++;
-            }
+        char c=s.charAt(l);
+        m2.put(c,m2.getOrDefault(c,0)-1);
+        if(m1.containsKey(c)&& m2.get(c)<m1.get(c)){
+          count--;
+        
         }
+        l++;
+        }
+    }
+    return minLen == Integer.MAX_VALUE ? "" : s.substring(si, si + minLen);
         
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(startIdx, startIdx + minLen);
     }
 }
