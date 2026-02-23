5280a981b516505274acd0f4b1895cc4d7528e77
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2026-02-23 05:43:40 +0530

    Time: 16 ms (60.13%), Space: 47.5 MB (41.8%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,45 +1,54 @@
 import java.util.*;
+
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) return "";
-      
-    HashMap<Character,Integer> m1=new HashMap<>();
-    HashMap<Character,Integer> m2=new HashMap<>();
-    int l1=s.length();
-    int l2=t.length();
-    for(int i=0;i<l2;i++){
-        char ch=t.charAt(i);
-        // m1.put(map.getOrDefault(ch,0)+1);
-         m1.put(ch,m1.getOrDefault(ch,0)+1);
-    }
-    int need=m1.size();
-    int l=0;
-    int minLen = Integer.MAX_VALUE;
-    int count=0;
-    int si=-1;
 
-    for(int r=0;r<l1;r++){
-        char ch=s.charAt(r);
-        m2.put(ch,m2.getOrDefault(ch,0)+1);
-        if(m1.containsKey(ch)&&m1.get(ch).intValue()==m2.get(ch).intValue()){
-            count++;
-            // m2.put(ch,m2.getOrDefault(ch,0)-1);
+        if (s.length() < t.length()) return "";
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char ch : t.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        while(count==need){
-            if(r-l+1<minLen){
-                minLen=r-l+1;
-                si=l;
+
+        int i = 0, j = 0;
+        int count = 0;
+        int minLen = Integer.MAX_VALUE;
+        int startIndex = -1;
+
+        while (j < s.length()) {
+
+            char endChar = s.charAt(j);
+
+            if (map.containsKey(endChar)) {
+                if (map.get(endChar) > 0) {
+                    count++;
+                }
+                map.put(endChar, map.get(endChar) - 1);
             }
-        char c=s.charAt(l);
-        m2.put(c,m2.getOrDefault(c,0)-1);
-        if(m1.containsKey(c)&& m2.get(c)<m1.get(c)){
-          count--;
-        
+
+            while (count == t.length()) {
+
+                if (j - i + 1 < minLen) {
+                    minLen = j - i + 1;
+                    startIndex = i;
+                }
+
+                char startChar = s.charAt(i);
+
+                if (map.containsKey(startChar)) {
+                    map.put(startChar, map.get(startChar) + 1);
+                    if (map.get(startChar) > 0) {
+                        count--;
+                    }
+                }
+
+                i++;
+            }
+
+            j++;
         }
-        l++;
-        }
-    }
-    return minLen == Integer.MAX_VALUE ? "" : s.substring(si, si + minLen);
-        
+
+        return startIndex == -1 ? "" : s.substring(startIndex, startIndex + minLen);
     }
 }
