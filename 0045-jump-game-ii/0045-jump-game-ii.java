f2e8644fa6b48eee53b74140c2a6bdd9e1adc6b4
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2026-03-10 21:11:56 +0530

    Time: 1 ms (99.6%), Space: 47.6 MB (17.41%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,20 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
-        int n=nums.length;
-       int r=0;
-       int l=0;
-       int c=0;
-     
-       while(r<n-1){
-          int far=0;
-       for(int i=l;i<=r;i++){
-        far=Math.max(far,i+nums[i]);
-       }
-       l=r+1;
-       r=far;
-       c++;
-        
-       }
-       return c;
+      int n=nums.length;
+      int l=0;
+      int r=0;
+      int jump=0;
+      while(r<n-1){
+        int far=0;
+        for(int i=l;i<=r;i++){
+            far=Math.max(far,i+nums[i]);
+        }
+        l=r+1;
+        r=far;
+        jump++;
+      }
+      return jump;
     }
 }
