ad37d31a9ce3dcc15dae30fb2fc57291d3fa455a
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2026-03-14 10:00:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -3,24 +3,12 @@ import java.util.Arrays;
 
 class Solution {
     public int[][]  insert(int[][] intervals, int[] newInterval) {
-       int n=intervals.length;
-       int i=0;
-       ArrayList<int[]> res=new ArrayList<>();
-    while(i<n&&intervals[i][1]<newInterval[0]){
-        res.add(intervals[i]);
-        i++;
-       }
-      while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-            i++;
+        ArrayList<int[][]> res=new ArrayList<>();
+        int l=intervals.length;
+        int i=0;
+        while(i<l&&intervals[i][1]<newInterval[0]){
+            res.add(intervals[i][])
         }
-        res.add(newInterval);
-       while(i<n&&intervals[i][0]>newInterval[1]){
-        res.add(intervals[i]);
-        i++;
-       }
-       return res.toArray(new int[res.size()][]);
-
+     
     }
 }
