807430027a596787694b2478c5199e0839e1c844
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2026-03-14 10:21:28 +0530

    Time: 1 ms (97.76%), Space: 47.1 MB (72.22%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -3,12 +3,27 @@ import java.util.Arrays;
 
 class Solution {
     public int[][]  insert(int[][] intervals, int[] newInterval) {
-        ArrayList<int[][]> res=new ArrayList<>();
+        ArrayList<int[]> res=new ArrayList<>();
         int l=intervals.length;
         int i=0;
         while(i<l&&intervals[i][1]<newInterval[0]){
-            res.add(intervals[i][])
+            res.add(intervals[i++]);
         }
+        while(i<l&&intervals[i][0]<=newInterval[1]){
+            newInterval[0]=Math.min(intervals[i][0],newInterval[0]);
+             newInterval[1]=Math.max(intervals[i][1],newInterval[1]);
+             i++;
+        }
+        res.add(newInterval);
+         while (i < intervals.length) {
+            res.add(intervals[i++]);
+        }
+
+      int ar[][]=new int[res.size()][2];
+      for(int j=0;j<res.size();j++){
+        ar[j]=res.get(j);
+      }
+      return ar;
      
     }
 }
