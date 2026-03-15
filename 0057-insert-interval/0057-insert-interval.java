2555fa9945d76ae9cb2b6de4b655ca5399a13f7b
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2026-03-15 08:50:54 +0530

    Time: 1 ms (97.81%), Space: 47.1 MB (71.83%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -3,27 +3,27 @@ import java.util.Arrays;
 
 class Solution {
     public int[][]  insert(int[][] intervals, int[] newInterval) {
-        ArrayList<int[]> res=new ArrayList<>();
-        int l=intervals.length;
-        int i=0;
-        while(i<l&&intervals[i][1]<newInterval[0]){
-            res.add(intervals[i++]);
-        }
-        while(i<l&&intervals[i][0]<=newInterval[1]){
-            newInterval[0]=Math.min(intervals[i][0],newInterval[0]);
-             newInterval[1]=Math.max(intervals[i][1],newInterval[1]);
-             i++;
-        }
-        res.add(newInterval);
-         while (i < intervals.length) {
-            res.add(intervals[i++]);
-        }
-
-      int ar[][]=new int[res.size()][2];
-      for(int j=0;j<res.size();j++){
+       ArrayList<int[]> res=new ArrayList<>();
+       int i=0;
+       int l=intervals.length;
+       while(i<l&&intervals[i][1]<newInterval[0]){
+        res.add(intervals[i]);
+        i++;
+       }
+       while(i<l&&intervals[i][0]<=newInterval[1]){
+        newInterval[0]=Math.min(newInterval[0],intervals[i][0]);
+          newInterval[1]=Math.max(newInterval[1],intervals[i][1]);
+          i++;
+       }
+       res.add(newInterval);
+       while(i<l){
+        res.add(intervals[i]);
+        i++;
+       }
+       int ar[][]=new int[res.size()][2];
+       for(int j=0;j<res.size();j++){
         ar[j]=res.get(j);
-      }
-      return ar;
-     
+       }
+       return ar;
     }
 }
