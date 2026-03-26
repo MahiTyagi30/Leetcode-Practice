bf5c938e105978a763cd557e7f262618d79cafd8
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2026-03-26 15:59:25 +0530

    Time: 0 ms (100%), Space: 43.5 MB (57.33%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,52 +1,30 @@
 class Solution {
-    public int fun(int[][] ar,int i,int j){
-        
-        if(i==0&&j==0){
+     public int solve(int m,int n,int[][] dp,int[][] obstacleGrid){
+           if (m >= 0 && n >= 0 && obstacleGrid[m][n] == 1) return 0;
+        if(m==0&&n==0){
             return 1;
         }
-        if(i<0||j<0){
+        if(m<0||n<0){
             return 0;
         }
-        if(ar[i][j]==1){
-            return 0;
+      
+        if(dp[m][n]!=-1){
+            return dp[m][n];
         }
-        
-        int a=fun(ar,i-1,j);
-        
-       
-          int  b=fun(ar,i,j-1);
-        
-        
-        return a+b;
+        int up=solve(m-1,n,dp,obstacleGrid);
+        int left=solve(m,n-1,dp,obstacleGrid);
+        return dp[m][n]=up+left;
     }
+   
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
-         int n=obstacleGrid[0].length;
-        // int a=fun(obstacleGrid,m-1,n-1);
-        // return a;
-        int[][] dp=new int[m][n];
-        dp[0][0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0; // No path through an obstacle
-                }
-                // if(i==0&&j==0){
-                //          dp[i][j]=1;
-                // }
-               
-                else{
-
-                
-                if(i>0){
-                    dp[i][j]+=dp[i-1][j];
-                }
-                if(j>0){
-                    dp[i][j]+=dp[i][j-1];
-                }
-                }
-            }
-        }
-        return dp[m-1][n-1];
+        int n=obstacleGrid[0].length;
+        int dp[][]=new int[m][n];
+   for(int i=0;i<m;i++){
+    for(int j=0;j<n;j++){
+        dp[i][j]=-1;
+    }
+   }
+     return solve(m-1,n-1,dp,obstacleGrid);
     }
 }
