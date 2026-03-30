0fd3ef0f7491c8b5a6a68ddc0606802da4302dec
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2026-03-30 10:01:03 +0530

    Time: 18 ms (41.29%), Space: 47.2 MB (22.84%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,26 +1,31 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-       int dp[][]=new int[n+1][amount+1];
-       for(int i=0;i<=n;i++){
-        dp[i][0]=0;
-       }
-       for(int i=1;i<=amount;i++){
-        dp[0][i]=Integer.MAX_VALUE-1;
-       }
-       for(int i=1;i<=n;i++){
-        for(int j=1;j<=amount;j++){
-            if(coins[i-1]<=j){
-                dp[i][j]=Math.min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
 
-            }
-            else{
-                dp[i][j]=dp[i-1][j];
+      int dp[][]=new int[n][amount+1];
+      for(int i=0;i<n;i++){
+        dp[i][0]=0;
+      }
+       for (int j = 1; j <= amount; j++) {
+            if (j % coins[0] == 0) {
+                dp[0][j] = j / coins[0];
+            } else {
+                dp[0][j] = (int)1e9;
             }
         }
-        
-       }
-       int res=dp[n][amount];
-        return res>=Integer.MAX_VALUE-1?-1:res;
+      for(int i=1;i<n;i++){
+        for(int j=1;j<=amount;j++){
+            int notTake = dp[i - 1][j];
+
+                int take = (int)1e9;
+                if (coins[i] <= j) {
+                    take = 1 + dp[i][j - coins[i]];
+                }
+
+                dp[i][j] = Math.min(take, notTake);
+        }
+      }
+      int res=dp[n-1][amount];
+        return res >= (int)1e9 ? -1 : res;
     }
 }
