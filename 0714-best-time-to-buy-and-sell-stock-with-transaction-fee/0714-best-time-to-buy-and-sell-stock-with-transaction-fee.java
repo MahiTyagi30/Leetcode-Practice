82364cc2528c004b60ca1dfa594b8bd9d6c9dfb6
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2026-04-11 09:14:41 +0530

    Time: 17 ms (48.51%), Space: 70.3 MB (56.25%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,36 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-    int n=prices.length;
-    int dp[][]=new int[n][2];
-    for(int i=0;i<dp.length;i++){
-        dp[i][0]=dp[i][1]=-1;
-    }
-    return dfs(dp,prices,1,n,fee,0);
-       
-    }
-public static int dfs(int[][] dp,int[] prices,int buy,int n,int fees,int idx)
-{
-    int maxprofit=0;
-    if(idx>=n){
-        return 0;
-    }
-    if(dp[idx][buy]!=-1){
-        return dp[idx][buy];
-    }
-    if(buy==1){
-      int a=-prices[idx]-fees+dfs(dp,prices,0,n,fees,idx+1);
-      int b=0+dfs(dp,prices,1,n,fees,idx+1);
-      maxprofit=Math.max(a,b);
-    }
-    else{
-         int a=prices[idx]+dfs(dp,prices,1,n,fees,idx+1);
-      int b=0+dfs(dp,prices,0,n,fees,idx+1);
-      maxprofit=Math.max(a,b);
-
-    }
     
-    return dp[idx][buy]=maxprofit;
-}
+       int n=prices.length;
+        int dp[][]=new int[n+1][2];
+        dp[n][0]=dp[n][1]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                int p=0;
+                if(j==1){
+                    p=Math.max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
+                }
+                else{
+                     p=Math.max(prices[i]+dp[i+1][1]-fee,0+dp[i+1][0]);
+                }
+                dp[i][j]=p;
+            }
+        }
+       
+         return dp[0][1];
+    }
 
  
 }
