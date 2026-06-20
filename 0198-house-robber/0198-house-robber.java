6147b5b574f2b3e4a56de8e22dfa996e4c2a5a23
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2026-06-20 17:55:19 +0530

    Time: 0 ms (100%), Space: 42.6 MB (73.57%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -19,8 +19,16 @@ class Solution {
         int[] dp = new int[n];
 
       
-       Arrays.fill(dp,-1);
+      dp[0]=nums[0];
+      for(int i=1;i<n;i++){
+        int pick=nums[i];
+        if(i>1){
+            pick=pick+dp[i-2];
+        }
+        int notpick=dp[i-1];
+        dp[i]=Math.max(pick,notpick);
 
-        return solve(n - 1, nums, dp);
+      }
+      return dp[n-1];
     }
 }
