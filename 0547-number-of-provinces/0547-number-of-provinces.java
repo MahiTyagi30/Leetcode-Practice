1438de6d39f0e8d6f96811c482a4246cfe2fa071
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2026-09-01 10:45:07 +0530

    Time: 3 ms (26.31%), Space: 49.3 MB (15.7%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,47 +1,38 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        List<List<Integer>> adj = adjListConvertion(isConnected);
-        int vertex = adj.size();
-        boolean vis[] =  new boolean[vertex];
-        int ans = 0;
-
-       for(int i=0;i<vertex;i++){
-        if(!vis[i]){
-            ans++;
-            dfsHelper(adj,i,vis);
-        }
-       }
-       return ans;      
-        
-    }
-
-    public  void  dfsHelper(List<List<Integer>> adjList,int node,boolean[] vis) {
-        vis[node]=true;
-
-        for(Integer it:adjList.get(node)){
-            if(!vis[it])
-                dfsHelper(adjList, it, vis);
-        }
-
-    }
-
-    public List<List<Integer>> adjListConvertion(int [][]isConnected){
-        
-        int n = isConnected.length;
-        int m = isConnected[0].length;
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++)
-        adj.add(new ArrayList<>());
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(isConnected[i][j]==1 && i!=j){
-                    adj.get(i).add(j);
-                }
-
+    public void dfs(int node,ArrayList<ArrayList<Integer>> adj,int []vis){
+        vis[node]=1;
+        for(Integer it:adj.get(node)){
+            if(vis[it]==0){
+                dfs(it,adj,vis);
             }
         }
-        return adj;
-
     }
-}
+    public int findCircleNum(int[][] isConnected) {
+        int V=isConnected.length;
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<Integer>());
+        }
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(isConnected[i][j]==1&&i!=j){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
+            }
+        }
+        int[] vis=new int[V];
+        int c=0;
+        for(int i=0;i<V;i++){
+            if(vis[i]==0){
+                c++;
+                dfs(i,adj,vis);
+            }
+        }
+        return c;
+       
+    }
+
+    
+
+  }
