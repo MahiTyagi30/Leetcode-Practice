4ddd9861ff2121e656abeffb3022f9d1cd487e47
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2026-09-03 10:54:57 +0530

    Time: 3 ms (9.67%), Space: 44.9 MB (5.72%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,61 +1,76 @@
 class Solution {
-   class Pair{
-    int row;
-    int col;
-    int time;
-    Pair(int row,int col,int time){
-        this.row=row;
-        this.col=col;
-        this.time=time;
+    public class Pair{
+        int r;
+        int c;
+        int time;
+        public Pair(int r,int c,int time){
+            this.r=r;
+            this.c=c;
+            this.time=time;
+        }
     }
-   }
-    public int orangesRotting(int[][] grid) {
-       int n=grid.length;
-       int m=grid[0].length;
-       int [][] vis=new int[n][m];
-       Queue<Pair> q=new LinkedList<>();
 
-       int cfresh=0;
-       for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
+  
+    public int orangesRotting(int[][] grid) {
+       ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+       int n=grid.length;
+    //    for(int i=0;i<n;i++){
+    //     adj.add(new ArrayList<>());
+
+    //    }
+    //    for(int[] e:grid{
+    //     int u=e[0];
+    //     int v=e[1];
+    //     adj.get(u).add(v);
+    //     adj.get(v).add(u);
+    //    }
+    Queue<Pair> q=new LinkedList<>();
+    int t=0;
+    int fresh =0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<grid[0].length;j++){
             if(grid[i][j]==2){
                 q.add(new Pair(i,j,0));
-                vis[i][j]=2;
-
             }
-            else{
-                vis[i][j]=0;
-            }
-            if(grid[i][j]==1){
-                cfresh++;
-            }
+             if (grid[i][j] == 1) {
+                    fresh++;
+                }
         }
-       }
-       int mtime=0;
-       int c=0;
-       int rw[]={-1,0,1,0};
-       int cw[]={0,1,0,-1};
-       while(!q.isEmpty()){
-        int r=q.peek().row;
-        int cl=q.peek().col;
-        int t=q.peek().time;
-        mtime=Math.max(mtime,t);
-        q.remove();
-        for(int i=0;i<4;i++){
-            int nr=r+rw[i];
-            int nc=cl+cw[i];
-             if(nr>=0&&nr<n&&nc>=0&&nc<m&&vis[nr][nc]==0&&grid[nr][nc]==1){
-                 q.add(new Pair(nr,nc,t+1));
-                    vis[nr][nc]=2;
-                    c++;
-             }
-        }
-       }
-       if(c!=cfresh){
-        return -1;
-       }
-       return mtime;
+    }
+    int maxt=0;
 
+    while(!q.isEmpty()){
+        Pair p=q.poll();
+        int r=p.r;
+        int c=p.c;
+        int time=p.time;
+        int[][] dir={
+             {-1, 0},
+    {1, 0},
+    {0, -1},
+    {0, 1}
+        };
+    
+    for(int[] d:dir){
+        int nr=r+d[0];
+        int nc=c+d[1];
+        // int newt=time+d[2];
+        if(nr >= 0 && nr < n &&
+nc >= 0 && nc < grid[0].length&&grid[nr][nc]==1){
+            // time=time+1;
+            grid[nr][nc] = 2;
+               fresh--;
+            q.add(new Pair(nr,nc,time+1));
+            maxt=Math.max(time+1,maxt);
+        }
+    }
+
+    }
+    if (fresh > 0) {
+            return -1;
+        }
+    return maxt;
         
     }
+
 }
