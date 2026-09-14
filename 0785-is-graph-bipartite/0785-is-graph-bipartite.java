e2a15589ded65fd862ec06091a8f26e94647482a
Mahi Tyagi <111622147+MahiTyagi30@users.noreply.github.com>
2026-09-14 20:27:37 +0530

    Time: 0 ms (100%), Space: 47.3 MB (37.51%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,32 +1,36 @@
 class Solution {
-    public static boolean dfs( int n,int v, int [][] graph, int[] color,int c) {
-        
-        color[n] = c;
-        
-       
-            for (int neigh : graph[n]) {
-                if (color[neigh] == -1) {
-                    if(!dfs(neigh, v, graph, color,1-c)){
-                        return false;
-                    }
-                } 
-                else if (color[neigh] == c) {
-                    return false;
-                }
+    public static boolean dfs(int i,int col,int[] colors,int[][] graph,int v){
+        colors[i]=col;
+        for(int neigh:graph[i]){
+             if (colors[neigh] == -1) {
+            if(dfs(neigh,1-col,colors,graph,v)==false){
+                return false;
             }
-        // }
+             }
+            else if(colors[neigh]==col){
+                return false;
+            }
+
+
+        }
         return true;
     }
-
     public boolean isBipartite(int[][] graph) {
-        int v = graph.length;
-        int[] color = new int[v];
-        Arrays.fill(color, -1);
-        
-        for (int i = 0; i < v; i++) {
-            if (color[i] == -1) {
-                if (!dfs(i, v, graph, color,0)) {
-                    return false;
+                int v=graph.length;
+        // ArrayList<ArrayList<Integer>> list=new ArrayList<>();
+        // for(int i=0;i<v;i++){
+        //     int 
+
+        // }
+        int [] colors=new int[v];
+        for(int i=0;i<v;i++){
+            colors[i]=-1;
+
+        }
+        for(int i=0;i<v;i++){
+            if(colors[i]==-1){
+                if(dfs(i,0,colors,graph,v)==false){
+                    return false; 
                 }
             }
         }
